13049434f80ea1414957bc89ad94d942e6461133
Edward J. Andress <eddie_andress@hotmail.com>
2014-09-11 17:22:47 +0100

    backing up laptop

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,6 +3,7 @@ class UpvotesController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@post.upvotes.create
+		raise @post
 	end
 
 end
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,3 +1,5 @@
 class Upvote < ActiveRecord::Base
+
   belongs_to :post
+  
 end
